37921804dd2688f69f3270dbcbf7d904cd50daee
Rjoydip <joydipand@gmail.com>
2018-01-20 15:18:41 +0545

    monaco editor setup done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+
+declare const monaco: any;
+declare const require: any;
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  @ViewChild('editor') editorContent: ElementRef;
+
+  ngAfterViewInit() {
+    let onGotAmdLoader = () => {
+      // Load monaco
+      (<any>window).require.config({ paths: { 'vs': '/assets/monaco/vs' } });
+      (<any>window).require(['vs/editor/editor.main'], () => {
+        this.initMonaco();
+      });
+    };
+
+    // Load AMD loader if necessary
+    if (!(<any>window).require) {
+      const loaderScript = document.createElement('script');
+      loaderScript.type = 'text/javascript';
+      loaderScript.src = '/assets/monaco/vs/loader.js';
+      loaderScript.addEventListener('load', onGotAmdLoader);
+      document.body.appendChild(loaderScript);
+    } else {
+      onGotAmdLoader();
+    }
+  }
+  // Will be called once monaco library is available
+  initMonaco() {
+    var myDiv: HTMLDivElement = this.editorContent.nativeElement;
+    var editor = monaco.editor.create(myDiv, {
+      value: [
+        'function x() {',
+        '\tconsole.log("Hello world!");',
+        '}'
+      ].join('\n'),
+      language: 'javascript'
+    });
+  }
 }
