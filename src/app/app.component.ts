e50eb925c227fa0328a37fbaf9f352a2cc97d10e
Rjoydip <joydipand@gmail.com>
2018-01-20 16:53:59 +0545

    editor initial configuration done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
+import IPFS from 'ipfs';
+import Y from 'yjs';
+
+require('y-memory')(Y)
+require('y-array')(Y)
+require('y-text')(Y)
+require('y-ipfs-connector')(Y)
 
 declare const monaco: any;
 declare const require: any;
@@ -10,7 +17,12 @@ declare const require: any;
 })
 export class AppComponent {
 
+  private ipfs: any;
+  private editorDiv: HTMLDivElement;
+
   @ViewChild('editor') editorContent: ElementRef;
+
+  constructor() { }
 
   ngAfterViewInit() {
     let onGotAmdLoader = () => {
@@ -32,16 +44,72 @@ export class AppComponent {
       onGotAmdLoader();
     }
   }
+
+  repo() {
+    return 'ipfs/yjs-demo/' + Math.random();
+  }
+
+  ipfsInit(editorRef: any) {
+    this.ipfs = new IPFS({
+      repo: this.repo(),
+      EXPERIMENTAL: {
+        pubsub: true
+      },
+      config: {
+        Addresses: {
+          Swarm: [
+            '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star'
+          ]
+        }
+      }
+    });
+
+    this.ipfs.once('ready', () => this.ipfs.id((err, info) => {
+      if (err) { throw err }
+
+      console.log('IPFS node ready with address ' + info.id)
+
+      Y({
+        db: {
+          name: 'memory'
+        },
+        connector: {
+          name: 'ipfs',
+          room: 'ipfs-yjs-demo',
+          ipfs: this.ipfs
+        },
+        share: {
+          textfield: 'Text'
+        }
+      }).then((y) => {
+        // y.share.textfield.bind(this.editorDiv);
+      })  
+    }))
+  }
+
   // Will be called once monaco library is available
   initMonaco() {
-    var myDiv: HTMLDivElement = this.editorContent.nativeElement;
-    var editor = monaco.editor.create(myDiv, {
+    this.editorDiv = this.editorContent.nativeElement;
+    const editor = monaco.editor.create(this.editorDiv, {
       value: [
-        'function x() {',
-        '\tconsole.log("Hello world!");',
-        '}'
+        'console.log("Hello world!");'
       ].join('\n'),
-      language: 'javascript'
+      language: 'javascript',
+      theme: 'vs-dark', // 'vs' | 'vs-dark' | 'hc-black'
+      minimap: {
+        enabled: false, // boolean
+        maxColumn: 120, // number
+        showSlider: "always", // "always" | "mouseover"
+      },
+      automaticLayout: true, // boolean
+      wordWrap: 'bounded', // 'off' | 'on' | 'wordWrapColumn' | 'bounded';
+      lineNumbers: 'on', //  'on' | 'off' | 'relative' | ((lineNumber: number) => string);
+      lineDecorationsWidth: 30, // number | string;
+      fontSize: 16, // number 
+      fontWeight: '500' // 'normal' | 'bold' | 'bolder' | 'lighter' | 'initial' | 'inherit' | '100' | '200' | '300' | '400' | '500' | '600' | '700' | '800' | '900';
     });
+
+    this.ipfsInit(editor);
   }
+
 }
